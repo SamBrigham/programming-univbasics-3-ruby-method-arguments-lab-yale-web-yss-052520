5879faa4800ce3e4d6ceef72430bb0c57b56ba79
Samuel Brigha <sam.brigham@yale.edu>
2020-05-08 16:18:38 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,22 +6,15 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-introduction("Sam")
+
+
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language("Sam", "Ruby")
 
 def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name}, and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language_optional("Sam")
-  
-
-
-
-
-
 
 
 
